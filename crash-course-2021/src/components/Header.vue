f79d4318adfad7fe3fa0806944c09f9ea1e13c1d
apiwatdev <82103342+apiwatdev@users.noreply.github.com>
2021-09-14 22:04:26 +0700

    component tasks ,task & emit function

--- crash-course-2021/src/components/Header.vue
+++ crash-course-2021/src/components/Header.vue
@@ -1,17 +1,32 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
+    <Button @click="onClick()" text="Add Task" color="green"/>
   </header>
 </template>
 
 <script>
+import Button from './Button.vue';
 export default {
+  components: { Button },
   name: "Header",
   props: {
     title: String,
   },
+  methods: {
+    onClick(){
+      console.log("click")
+    }
+  }
+
 };
 </script>
 
-<style>
+<style scope>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
 </style>
---
+++ crash-course-2021/src/components/Tasks.vue
@@ -0,0 +1,21 @@
+  
+<template>
+  <div>
+    <div :key="task.id" v-for="task in tasks">
+     <task @delete-task="$emit('delete-task',task.id)" :task="task" />   
+    </div>
+  </div>
+</template>
+<script>
+import Task from './Task.vue';
+
+export default {
+  components: { Task },
+  props: {
+    tasks: Array,
+  },
+};
+</script>
+
+<style>
+</style>
